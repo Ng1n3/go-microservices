c46f29b0ef5088932bf4e8ec99603836bda77789
Ng1n3 <code.with.muyiwa@gmail.com>
2025-05-25 13:33:10 +0000

    feat: setup frontend to communicate with broker via logger

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -16,11 +16,17 @@ type jsonReponse struct {
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -45,9 +51,47 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
 
+	case "log":
+		app.logItem(w, requestPayload.Log)
+
 	default:
 		app.errorJSON(w, errors.New("unkown action"))
+
 	}
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonReponse
+	payload.Error = false
+	payload.Message = "logged"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, authPayload AuthPayload) {
@@ -77,7 +121,6 @@ func (app *Config) authenticate(w http.ResponseWriter, authPayload AuthPayload) 
 		app.errorJSON(w, errors.New("error calling auth service"))
 		return
 	}
-
 
 	var jsonFromService jsonReponse
 
