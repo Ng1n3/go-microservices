4422874bcd0d2575aea553a98e5b69e896e2f384
Ng1n3 <code.with.muyiwa@gmail.com>
2025-05-26 19:30:14 +0000

    feat: setup rabbitMQ in docker compose to get image

---
+++ listener-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/Ng1n3/go-microservices/listener-service
+
+go 1.22.3
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
---
+++ listener-service/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+  // try to connect to rabbitmq
+
+
+  // start listening to messages
+
+  // create consumer
+
+  // watch the queue and consume events
+
+  
+}
